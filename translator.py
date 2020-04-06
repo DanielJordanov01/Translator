f82638861d9d48e96d2c7c42518522541f201810
Даниел Валентинов <daniel159696@gmail.com>
2020-04-06 16:12:19 +0300

    initial commit

---
+++ translator.py
@@ -0,0 +1,16 @@
+from googletrans import Translator
+import sys
+
+# HOW TO RUN: python translator.py (file) (source language (user language abbreviation - en, de, ja...) )
+# (destination language (user language abbreviation - en, de, ja...))')
+
+translator = Translator()
+
+try:
+    with open(sys.argv[1]) as text_file:
+        original_text = text_file.read()
+        translation = translator.translate(original_text, dest=sys.argv[2])
+        with open('translated_text.txt', 'w', encoding='utf-16') as translated_text:
+            translated_text.write(translation.text)
+except FileNotFoundError :
+    print('File not found!')
